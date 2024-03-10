a3203aeb76e9b1d24e9b609500c51feb1efb1f87
Ocheretko1992 <117608037+Ocheretko1992@users.noreply.github.com>
2024-03-10 17:14:47 +0200

    update

--- src/js/marckup.js
+++ src/js/marckup.js
@@ -3,7 +3,24 @@ export const marckupOneTodo = ({status, id, text}) => {
     const btn = status === "todo" ? buttonUpdate : buttonDelete;
     return `<li class="${status}" id="${id}"><p>${text}</p>${btn}</li>`;
 }
-const addAllTodo = (data) => {
+export const addAllTodo = (data) => {
     const marckup = data.map(marckupOneTodo).join("");
     todoList.innerHTML = marckup;
 } 
+
+
+
+
+
+
+//*Зміна статусу кнопки в інтерфейсі !!!
+
+// if (element.classList.contains('todo')) {
+//     element.classList.replace('todo', 'complete');
+//     element.lastElementChild.remove();
+//     element.insertAdjacentHTML('beforeend', buttonDelete);
+//   } else {
+//     element.classList.replace('complete', 'todo');
+//     element.lastElementChild.remove();
+//     element.insertAdjacentHTML('beforeend', buttonUpdate);
+//   }
